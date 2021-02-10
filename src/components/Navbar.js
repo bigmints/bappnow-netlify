c0f7bba4c8a1944b77fce61740dd9fa4f3aa999e
bigmints <66945811+bigmints@users.noreply.github.com>
2021-02-10 23:46:04 +0400

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -64,7 +64,7 @@ const Navbar = class extends React.Component {
                 About
               </Link>
               <Link className="navbar-item" to="/products">
-                Products
+                Products asdasd
               </Link>
               <Link className="navbar-item" to="/blog">
                 Blog
